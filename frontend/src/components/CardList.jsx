612fb21fb706786cb8ca12ef68131b9b03efe534
huytdn <saolailatoivay991@gmail.com>
2025-10-02 13:22:12 +0700

    cardlist + footer

---
+++ frontend/src/components/CardList.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import CardImg from "../assets/images/cardimg.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+const CardList = () => {
+  const data = [
+    {
+      id: 1,
+      title: "Card  1",
+      description: "Description for Card 1",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 2,
+      title: "Card  2",
+      description: "Description for Card 2",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Card  3",
+      description: "Description for Card 3",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+  ];
+
+  return (
+    <div className="text-white md:px-4">
+      <h2 className="pt-10 pb-5 text-lg font-medium">Upcoming</h2>
+
+      <Swiper slidesPerView={"auto"} className="mySwiper">
+        {data.map((item, index) => (
+          <SwiperSlide key={index} className="max-w-72">
+            <img
+              src={CardImg}
+              alt=""
+              className="h-44 w-full object-center object-cover"
+            />
+            <p className="text-center pt-2">{item.title}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default CardList;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import CardList from "../components/CardList";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <div className="p-5">
       <Hero />
+      <CardList />
+      <CardList />
+      <CardList />
+      <Footer />
     </div>
   );
 };
