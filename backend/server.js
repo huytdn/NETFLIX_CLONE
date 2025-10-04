81b3cacec8258fdfe01e8cb53dc1a264b7a04a78
huytdn <saolailatoivay991@gmail.com>
2025-10-04 23:04:25 +0700

    signup testing

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,68 @@
 import express from "express";
 import { connectToDB } from "./config/db.js";
 import dotenv from "dotenv";
+import User from "./models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("NhatHuy");
+});
+
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      throw new Error("All fieldss are required");
+    }
+
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    const usernameExist = await User.findOne({ username });
+    if (usernameExist) {
+      return res
+        .status(400)
+        .json({ message: "Username is taken, try another name." });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const userDoc = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // JWT
+
+    if (userDoc) {
+      // jwt.sign(payload, secret, options)
+      const token = jwt.sign({ id: userDoc._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ user: userDoc, message: "User created successfully." });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(PORT, () => {
